9076841e8d416c7fd3c94dd63b5708c9aa565334
ayushi7572 <ayushiaggarwal17@gmail.com>
2022-11-06 23:06:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Header from './app/Header';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Let's build a Linkedin Clone</h1>
+
+     {/*App Header*/}
+     <Header/>
+
+     {/*Sidebar */}
+
+
+     {/*Feed */}
+
+
+     {/*Widgets */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css';
+function Header() {
+  return (
+    <div className='header'>
+        <h1>This is the Header</h1>
+
+     <div classname=".header__left">
+
+     </div>
+     <div className=".header__right">
+
+     </div>
+
+    </div>
+    
+  )
+}
+
+export default Header
